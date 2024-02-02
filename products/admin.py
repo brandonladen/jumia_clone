81b16997a83085dc55b5e4357eef00c2adee6d5d
brandonladen <brandonladen486@gmail.com>
2024-02-02 20:31:27 +0300

    Updated models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Product, Prod_Category, Order
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Prod_Category)
+admin.site.register(Order)
--- products/models.py
+++ products/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class Prod_Category(models.Model):
     Categ_Name = models.CharField(max_length=50)
     
-class Poduct(models.Model):
+class Product(models.Model):
     Category = models.ForeignKey('Prod_Category', on_delete=models.CASCADE)
     Prod_Name = models.CharField(max_length=50)
     Prod_Image = models.ImageField(upload_to = "path")
-    Prod_Image_Desc = models.ImageField(upload_to = "path", null=True, blank=True)
     Pod_Price = models.IntegerField()
     Pod_Description = models.TextField()
+    
+class Order(models.Model):
+    Order_ID = models.CharField(max_length=50)
+    Product_Name = models.ForeignKey('Product', on_delete=models.CASCADE)
+    Date_Time = models.DateTimeField(auto_now_add=True)
