7cae4aa2c7fea4f323d4ba4e5c8957b017e07b7e
brandonladen <brandonladen486@gmail.com>
2024-02-03 01:34:07 +0300

    index items list by category

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Prod_Category, Product, Order
+from operator import attrgetter
 # Create your views here.
 
 def home(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('Category')
     context = {
         'products': products
     }
