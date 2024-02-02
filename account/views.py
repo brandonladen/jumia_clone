a7015e64c234476e32c3d88af0a0155adb39b42f
brandonladen <brandonladen486@gmail.com>
2024-02-03 00:22:02 +0300

    account mngt

--- account/views.py
+++ account/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Customer
-from .forms import CustomerCreationForm
+from .forms import CustomerCreationForm, CustomerLoginForm
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+from django.urls import reverse
 
 # Create your views here.
 def Customer_signUp(request):
@@ -8,15 +11,35 @@ def Customer_signUp(request):
         form = CustomerCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            redirect('login')
+            return redirect(reverse('login'))
     else:
         form = CustomerCreationForm()
         
     context = {
         'form': form
     }
-    return render(request, "")
+    return render(request, "account/CustomerCreationForm.html", context)
         
 
 def Customer_login(request):
-    pass
+    if request.method == 'POST':
+        form = CustomerLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Successfully logged in.')
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = CustomerLoginForm()
+    return render(request, 'account/CustomerLoginForm.html', {'form': form})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Successfully logged out.')
+    return redirect('login')
