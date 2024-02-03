ea793c21f999149ef7cea1e41cf26bf70ce77e73
brandonladen <brandonladen486@gmail.com>
2024-02-03 05:47:38 +0300

    added login decorator

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Prod_Category, Product, Order
 from operator import attrgetter
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def home(request):
     products_all = Product.objects.all()
     products = Product.objects.all().order_by('Category')
@@ -13,6 +15,7 @@ def home(request):
     
     return render(request, "products/index.html", context)
 
+@login_required
 def category_page(request, cat_id):
     categories = Product.objects.filter(Category=cat_id)
     category_name = Prod_Category.objects.filter(pk=cat_id).first()
