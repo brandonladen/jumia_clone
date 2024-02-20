61889fc1bc289b5c6519e326ff556ec9fef6f6bd
brandonladen <brandonladen486@gmail.com>
2024-02-20 20:32:35 +0300

    cart updates

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, category_page
+from .views import home, category_page, cart_page
 
 urlpatterns = [
     path('', home, name='home'),
     path('categories/<int:cat_id>/', category_page, name='category'),
+    path('cart/<int:prod_id>', cart_page, name='cart'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Prod_Category, Product, Order
 from operator import attrgetter
 from django.contrib.auth.decorators import login_required
@@ -20,3 +20,11 @@ def category_page(request, cat_id):
     categories = Product.objects.filter(Category=cat_id)
     category_name = Prod_Category.objects.filter(pk=cat_id).first()
     return render(request, "products/category.html", {'categories' : categories, 'category_name': category_name})
+
+@login_required
+def cart_page(request, prod_id):
+    product = get_object_or_404(Product, pk=prod_id)
+    context = {
+        'product' : product,
+    }
+    return render(request, 'products/cart.html', context)
